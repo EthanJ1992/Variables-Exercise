1db97029bcf3e33008010bb81fc45e1ffafa6b97
Ethan Jacobs <jacobethan1992@gmail.com>
2023-01-31 11:13:53 -0500

    done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int attitude = 7;
+            string ralphPersonality = "Introvert";
+            char hashTag = '#';
+            bool isAntisocial = true;
+            double myHeight = 5.9;
+            decimal myPrice = 29.19921m;
+
+
+            Console.WriteLine($"ralph has a {attitude} out of 10 attitude and he's an {ralphPersonality} ");
+            Console.WriteLine($"Ralph has a {attitude} out of 10 attitude and he's an {isAntisocial}");
+            Console.WriteLine($"my height is {myHeight}");
+            Console.WriteLine($"my price is {myPrice}");
         }
     }
 }
